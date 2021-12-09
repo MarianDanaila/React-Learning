9d387df628784814f58c74d517ab32e381b7452d
MarianDanaila <mariandanaila01@gmail.com>
2021-12-09 12:23:25 +0200

    Using the Effect Hook

---
+++ Hooks/using-the-effect-hook/src/Example.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+export default function Example() {
+  const [count, setCount] = useState(0);
+
+  // Similar to componentDidMount and componendDidUpdate
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
