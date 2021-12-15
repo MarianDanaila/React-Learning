5cc9ea3eb92a5e4949b156298cc1c97a1effe337
MarianDanaila <mariandanaila01@gmail.com>
2021-12-15 10:02:38 +0200

    Refactor files

---
+++ Docs/Advanced Guides/refs-and-the-dom/src/App.js
@@ -0,0 +1,13 @@
+import AutoFocusTextInput from "./AutoFocusTextInput";
+import FunctionComponent from "./FunctionComponent";
+
+function App() {
+  return (
+    <div>
+      <AutoFocusTextInput />
+      <FunctionComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ Docs/Advanced Guides/refs-and-the-dom/src/FunctionComponent.js
@@ -0,0 +1,16 @@
+import { useRef } from "react";
+
+export default function FunctionComponent() {
+  const textInput = useRef(null);
+
+  function handleClick() {
+    textInput.current.focus();
+  }
+
+  return (
+    <div>
+      <input type="text" ref={textInput} />
+      <input type="button" value="Focus the text input" onClick={handleClick} />
+    </div>
+  );
+}
